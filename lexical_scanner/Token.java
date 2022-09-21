989704add754fa49b760163f19f10efe06401eaf
Omar Ahmed Hesham Aziz <omarahmedheshamaziz@Omars-MacBook-Pro-2.local>
2022-09-21 23:37:21 +0200

    Expression Class Generator + AST Printer + RPN Printer

--- lexical_scanner/Token.java
+++ lexical_scanner/Token.java
@@ -16,4 +16,8 @@ public class Token {
     public String toString() {
         return type + " " + lexeme + " " + literal;
     }
+
+    public String getLexeme() {
+        return lexeme;
+    }
 }
---
+++ syntax_tree/RpnPrinter.java
@@ -0,0 +1,33 @@
+package syntax_tree;
+
+import lexical_scanner.TokenType;
+
+import java.util.Objects;
+
+public class RpnPrinter implements Expr.Visitor<String> {
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return expr.left.accept(this) + " " + expr.right.accept(this) + " " + expr.operator.getLexeme();
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return expr.expression.accept(this);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if(expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return Objects.equals(expr.operator.getLexeme(), TokenType.MINUS.name()) ?
+                expr.right.accept(this) + " ~" : expr.right.accept(this) + " " + expr.operator.getLexeme();
+    }
+}
