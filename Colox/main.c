865907f6381e79974c5678a8c1ac251b633fb67c
Omar Ahmed Hesham Aziz <omarahmedheshamaziz@Omars-MacBook-Pro-2.local>
2022-11-15 23:15:46 +0200

    Disassembler

--- Colox/main.c
+++ Colox/main.c
@@ -1,5 +1,12 @@
 #include <utils/common.h>
+#include <utils/debug.h>
+#include <bytecode/chunk.h>
 
 int main(int argc, const char* argv[]) {
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    disassembleChunk(&chunk, "test chunk");
+    freeChunk(&chunk);
     return 0;
 }
