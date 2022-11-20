f8d3cc4d774fb686cb4512d2e187010b870e5ed2
omaraziz255 <34421528+omaraziz255@users.noreply.github.com>
2022-11-20 15:07:33 +0200

    Stack-based operations (#12)
    Co-authored-by: Omar Ahmed Hesham Aziz <omarahmedheshamaziz@Omars-MacBook-Pro-2.local>

---
+++ Colox/src/virtual-machine/vm.c
@@ -0,0 +1,83 @@
+/** Created by Omar Ahmed Hesham Aziz on 18/11/2022 */
+#include <stdio.h>
+
+#include <utils/common.h>
+#include <virtual-machine/vm.h>
+#include <utils/debug.h>
+
+VM vm;
+
+static InterpretResult run() {
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+
+#define BINARY_OP(op) \
+    do {              \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while(false)
+
+
+    for(;;) {
+#ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+        switch (READ_BYTE()) {
+            case OP_CONSTANT: {
+                Value constant = READ_CONSTANT();
+                push(constant);
+                break;
+            }
+            case OP_ADD:        BINARY_OP(+);break;
+            case OP_SUBTRACT:   BINARY_OP(-);break;
+            case OP_MULTIPLY:   BINARY_OP(*);break;
+            case OP_DIVIDE:     BINARY_OP(/);break;
+            case OP_NEGATE:     *(vm.stackTop-1) *= -1; break;
+            case OP_RETURN:
+                printValue(pop());
+                printf("\n");
+                return INTERPRET_OK;
+        }
+    }
+
+#undef BINARY_OP
+#undef READ_CONSTANT
+#undef READ_BYTE
+}
+
+static void resetStack() {
+    vm.stackTop = vm.stack;
+}
+
+void initVM() {
+    resetStack();
+}
+
+void freeVM() {
+
+}
+
+InterpretResult interpret(Chunk* chunk) {
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
+
+void push(Value value) {
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop() {
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
