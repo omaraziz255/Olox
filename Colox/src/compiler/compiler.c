d3b9d461225da463d7b91c66f58fa4abd42ee96b
omar-hmarzouk <omar.marzouk@brightskiesinc.com>
2022-12-07 15:14:19 +0200

    Print Statement Support

--- Colox/src/compiler/compiler.c
+++ Colox/src/compiler/compiler.c
@@ -90,6 +90,16 @@ static void consume(TokenType type, const char* message) {
     errorAtCurrent(message);
 }
 
+static bool check(TokenType type) {
+    return parser.current.type == type;
+}
+
+static bool match(TokenType type) {
+    if(!check(type))    return false;
+    advance();
+    return true;
+}
+
 static void emitByte(uint8_t byte) {
     writeChunk(currentChunk(), byte, parser.previous.line);
 }
@@ -127,6 +137,8 @@ static void endCompiler() {
 }
 
 static void expression();
+static void statement();
+static void declaration();
 static ParseRule* getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
@@ -182,6 +194,22 @@ static void literal() {
 
 static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
+}
+
+static void printStatement() {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expected ; after semicolon");
+    emitByte(OP_PRINT);
+}
+
+static void declaration() {
+    statement();
+}
+
+static void statement() {
+    if(match(TOKEN_PRINT)) {
+        printStatement();
+    }
 }
 
 static void grouping() {
@@ -287,8 +315,11 @@ bool compile(const char* source, Chunk* chunk) {
     parser.hadError = false;
     parser.panicMode = false;
     advance();
-    expression();
-    consume(TOKEN_EOF, "Expected end of expression");
+
+    while(!match(TOKEN_EOF)) {
+        declaration();
+    }
+
     endCompiler();
     return !parser.hadError;
 }
--- Colox/src/virtual-machine/vm.c
+++ Colox/src/virtual-machine/vm.c
@@ -106,9 +106,13 @@ static InterpretResult run() {
 
                 *(vm.stackTop-1) = NUMBER_VAL(-AS_NUMBER(*(vm.stackTop-1)));
                 break;
-            case OP_RETURN:
+            case OP_PRINT: {
                 printValue(pop());
                 printf("\n");
+                break;
+            }
+            case OP_RETURN:
+                // Exit interpreter
                 return INTERPRET_OK;
         }
     }
