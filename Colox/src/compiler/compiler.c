0db2f5b079633f7297b00a35f8c851f4307a77b0
omar-hmarzouk <omar.marzouk@brightskiesinc.com>
2022-12-07 15:21:58 +0200

    Expression Statements

--- Colox/src/compiler/compiler.c
+++ Colox/src/compiler/compiler.c
@@ -196,6 +196,12 @@ static void expression() {
     parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void expressionStatement() {
+    expression();
+    consume(TOKEN_SEMICOLON, "Expected ; after expression");
+    emitByte(OP_POP);
+}
+
 static void printStatement() {
     expression();
     consume(TOKEN_SEMICOLON, "Expected ; after semicolon");
@@ -209,6 +215,9 @@ static void declaration() {
 static void statement() {
     if(match(TOKEN_PRINT)) {
         printStatement();
+    }
+    else {
+        expressionStatement();
     }
 }
 
